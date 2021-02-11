8fdfa8622d05b23bdbed676ceab097b027ecd9e9
Roberto Cesetti <roberto.cesetti@studenti.unicam.it>
2021-02-11 10:07:29 +0100

    Backend: rimosso da ControllerProgetto il metodo che restituiva i progetti personali.

--- backend/src/main/java/it/unicam/dmr/doit/controller/progetto/ControllerProgetto.java
+++ backend/src/main/java/it/unicam/dmr/doit/controller/progetto/ControllerProgetto.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.unicam.dmr.doit.controller.Utils;
-import it.unicam.dmr.doit.dataTransferObject.iscritto.RuoloDto;
 import it.unicam.dmr.doit.dataTransferObject.progetto.TagListDto;
 import it.unicam.dmr.doit.progetto.Progetto;
 import it.unicam.dmr.doit.service.progetto.ProgettoService;
@@ -65,18 +63,5 @@ public class ControllerProgetto {
 		if (bindingResult.hasErrors())
 			return Utils.creaMessaggioDaErrore(bindingResult);
 		return Utils.creaRisposta(progettoService.findByName(nome,tags), HttpStatus.OK);
-	}
-
-	@GetMapping("/personali")
-	public ResponseEntity<?> progettiPersonali(@Valid @RequestBody List<RuoloDto> ruoli, BindingResult bindingResult,
-			Authentication authentication) {
-		if (bindingResult.hasErrors())
-			return Utils.creaMessaggioDaErrore(bindingResult);
-		
-		try {
-			return Utils.creaRisposta(progettoService.findProgettiPersonali(authentication.getName(),ruoli), HttpStatus.OK);
-		} catch (NotFoundException e) {
-			return Utils.creaMessaggio(e, HttpStatus.NOT_FOUND);
-		}
 	}
 }
